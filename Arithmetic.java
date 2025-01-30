66a202d4e5231f6f3edb800e39ec30476872e596
Bhavaninaraga <naragabhavani@gmail.com>
2025-01-30 19:47:25 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,8 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.printf("Sum of %d+%d is : %d",a,b,a+b);
+    }
+    
+}
